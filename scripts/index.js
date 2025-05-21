61adbe863b08f922aa1a7ecfa30dbc329f7c03ec
Rich <TonyRiches17@yahoo.com>
2025-05-21 00:24:53 -0400

    Form Submit

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const bookForm = document.querySelector(".book__form");
 
-bookForm.addEventListener("click", (evt) => {
+bookForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
   console.log("it worked");
   alert("Thank you for your booking an appointment. I will reach out to confirm as date approaches");
